9948a232dfede955abecec7289adc62b7a2eb731
Buse Kaban <67025642+BuseKaban@users.noreply.github.com>
2023-06-19 22:52:37 +0300

    merge commit

--- template.js
+++ template.js
@@ -1,2 +1,30 @@
+<<<<<<< HEAD
 // alert("Deneme")
 // window.alert("Deneme")
+=======
+// Dark Mode 
+let darkModu=()=>{
+    //alert("Dark modu");
+    document.body.classList.toggle("dark_mode");
+}
+
+// input search
+$(document).ready(function(){
+const searchApi=["Adana","Balikesir","Çorum","Denizli","Diyarbakır","Edirne","Malatya","Van"];
+$("#tags").autocomplete({
+    source:searchApi
+})
+}); //end 
+
+
+// Footer
+let newDate=()=>{
+    const date=new Date().getFullYear();
+// JS DOM
+//document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+// Jquery DOM
+$("#date_id").html(date);
+}
+newDate()
+>>>>>>> 57ea3e45afc8a397d9eba6b4d85878563f9ebfc9
